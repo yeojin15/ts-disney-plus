ed397448d199426eba9f221212f7aa6bbe7b3fb8
yjkim <k.yeojin15@gmail.com>
2023-10-17 21:53:25 +0900

    feat : firebase init

--- src/Util/api.ts
+++ src/Util/api.ts
@@ -5,7 +5,7 @@ export const baseURL: string = 'https://image.tmdb.org/t/p/original/';
 export const requestApi: AxiosInstance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: process.env.REACT_APP_API_KEY,
+    api_key: process.env.REACT_APP_REQUEST_KEY,
     language: 'ko-KR',
   },
 });
