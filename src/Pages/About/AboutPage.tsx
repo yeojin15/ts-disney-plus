91713f225feb6f06d820dc7b03ed655c05211ff1
yjkim <k.yeojin15@gmail.com>
2023-10-23 11:11:34 +0900

    feat : about page

--- src/Pages/About/AboutPage.tsx
+++ src/Pages/About/AboutPage.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
+import { AboutWrap, Profile } from './About.style';
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  const [profile, setProfile] = useState(null);
+  // 유저 리스트 보여주고 추가할수있도록(가족계정) 최대 8개? 6개?
+  return (
+    <AboutWrap>
+      <Profile>
+        <div className='profile-img'>
+          {profile !== null ? (
+            <img src='' alt='user' />
+          ) : (
+            <div className='img' />
+          )}
+        </div>
+        <h2>Yeojin</h2>
+      </Profile>
+    </AboutWrap>
+  );
 };
 
 export default AboutPage;
